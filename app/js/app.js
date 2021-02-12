18e4270bf4f275211b923ad58a94df7e0a8ef9b5
Tania-frontend <tatyanagroshevaya@gmail.com>
2021-02-12 11:12:57 +0200

    added new file

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 	alert(1);
 });
+ var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+            delay: 60000,
+            disableOnInteraction: false
+  },
+ });
